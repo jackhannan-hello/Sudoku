f8ac24cd4a1e8c198c054bd365bc9eb187c6200f
[Jack Hannan] <[jack.hannan@mymail.champlian.edu]>
2023-02-28 10:20:45 -0500

    set up board correctly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value: null}
+  }
+  
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+
+class BigSquare extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+  
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    return (  
+      <div>
+        <div className='bigSquare'>
+          <div className="sqaure-row">
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+          </div>
+          <div className="square-row">
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+          </div>
+          <div className="square-row">
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Board extends React.Component {
+  renderBigSquare(i) {
+    return <BigSquare value={i}/>;
+  }
+
+  render() {
+    const status = 'SUDOKU';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className='board'>
+          <div className='board-row'>  
+            {this.renderBigSquare(0)}{this.renderBigSquare(1)}{this.renderBigSquare(2)}
+          </div>
+          <div className='board-row'>  
+            {this.renderBigSquare(3)}{this.renderBigSquare(4)}{this.renderBigSquare(5)}
+          </div>
+          <div className='board-row'>  
+            {this.renderBigSquare(6)}{this.renderBigSquare(7)}{this.renderBigSquare(8)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +110,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
-export default App;
+export default Game;
