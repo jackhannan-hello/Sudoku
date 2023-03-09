2d2063e6181bebdb696cda705473affa2214b1e5
[Jack Hannan] <[jack.hannan@mymail.champlian.edu]>
2023-03-09 15:12:15 -0500

    trying to get buttons work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,47 @@
 import './App.css';
 import React from 'react';
 
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {value: null}
-  }
-  
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+function Num(props) {
+  return (
+    <button className="num" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 
-class BigSquare extends React.Component {
+{/*class BigSquare extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       squares: Array(9).fill(null)
     }
   }
+
+  numberSelection = ""
   
+  placeNumber(i){
+    const squares = this.state.squares.slice();
+    squares[i] = this.numberSelection;
+    this.setState({squares: squares});
+  }
+
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.placeNumber(i)}
+      />
+    );
   }
 
   render() {
@@ -52,11 +67,69 @@ class BigSquare extends React.Component {
       </div>
     )
   }
-}
+}*/}
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+  
+  numberSelection = ""
+
+  selectNum(i) {
+    BigSquare.numberSelection = i
+  }
+
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.placeNumber(i)}
+      />
+    );
+  }
+
   renderBigSquare(i) {
-    return <BigSquare value={i}/>;
+    return (  
+      <div>
+        <div className='bigSquare'>
+          <div className="sqaure-row">
+            <div className='square0'>{this.renderSquare(0)}</div>
+            <div className='square1'>{this.renderSquare(1)}</div>
+            <div className='square2'>{this.renderSquare(2)}</div>
+          </div>
+          <div className="square-row">
+            <div className='square3'>{this.renderSquare(3)}</div>
+            <div className='square4'>{this.renderSquare(4)}</div>
+            <div className='square5'>{this.renderSquare(5)}</div>
+          </div>
+          <div className="square-row">
+            <div className='square6'>{this.renderSquare(6)}</div>
+            <div className='square7'>{this.renderSquare(7)}</div>
+            <div className='square8'>{this.renderSquare(8)}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderNum(i) {
+    return (
+      <Num 
+        value={i}
+        onClick={() => this.selectNum(i)}
+      />
+    );
   }
 
   render() {
@@ -76,6 +149,9 @@ class Board extends React.Component {
             {this.renderBigSquare(6)}{this.renderBigSquare(7)}{this.renderBigSquare(8)}
           </div>
         </div>
+        <div className='nums'>
+          {this.renderNum(1)}{this.renderNum(2)}{this.renderNum(3)}{this.renderNum(4)}{this.renderNum(5)}{this.renderNum(6)}{this.renderNum(7)}{this.renderNum(8)}{this.renderNum(9)}
+        </div>
       </div>
     );
   }
@@ -92,26 +168,5 @@ class Game extends React.Component {
     );
   }
 }
-
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
 
 export default Game;
